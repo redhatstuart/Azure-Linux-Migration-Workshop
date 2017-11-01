a4563f22f1971b2d1601e834df087060e28b38a1
Stuart Kirk <stuart.kirk@microsoft.com>
2017-10-31 23:46:08 -0400

    Add Logging

--- provision-scripts/lsprovision.sh
+++ provision-scripts/lsprovision.sh
@@ -8,6 +8,12 @@ chmod 755 /root/provision-stage1.sh
 chmod 755 /root/provision-stage2.sh
 chmod 755 /root/provision-stage3.sh
 
+echo "Calling Provision Stage 1 at `date`" >>/root/lsprovision.log
 bash /root/provision-stage1.sh
+echo "Provision Stage 1 script complete at `date`" >>/root/lsprovision.log
+echo "Calling Provision Stage 2 at `date`" >>/root/lsprovision.log
 bash /root/provision-stage2.sh
+echo "Provision Stage 2 script complete at `date`" >>/root/lsprovision.log
+echo "Calling Provision Stage 3 at `date`" >>/root/lsprovision.log
 bash /root/provision-stage3.sh
+echo "Provision Stage 3 script complete at `date`" >>/root/lsprovision.log
--- provision-scripts/provision-stage2.sh
+++ provision-scripts/provision-stage2.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
+echo "--BEGIN-- Provision Stage 2 Script at `date`" >>/root/lsprovision.log
 echo "********************************************************************************************"
-	echo "Creating migrate-host nested virtual machine"
+	echo "Creating migrate-host nested virtual machine at `date`" >>/root/lsprovision.log
 	virt-install --name migrate-host --ram 4096 --disk path=/var/lib/libvirt/images/migrate-host.qcow,format=qcow2,size=10,bus=scsi --location=/var/lib/libvirt/images/CentOS-7-x86_64-Minimal-1708.iso --hvm --vcpus 2 --os-type linux --os-variant centos7.0 --network network=default --nographics --noreboot --console pty,target_type=serial --initrd-inject=/var/lib/libvirt/images/migrate-host-ks.cfg --extra-args 'console=ttyS0,115200n8 serial ks=file:/migrate-host-ks.cfg'
+	echo "Nested VM install complete at `date`" >>/root/lsprovision.log
         sleep 10
+        echo "Configuring VM autostart at `date`" >>/root/lsprovision.log
 	virsh autostart migrate-host
+        echo "Starting Nested VM at `date`" >>/root/lsprovision.log
 	virsh start migrate-host
 	echo "VM install complete. Sleeping to determine IP address and populating student desktop"
 	sleep 10
 echo "********************************************************************************************"
-
-
-
-
-
-
-
-
-
-
-
-
+echo "--END-- Provision Stage 2 Script at `date`" >>/root/lsprovision.log
 
