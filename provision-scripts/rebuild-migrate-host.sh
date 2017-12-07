0372fa891cb7cd71fc01c9a0946b0bd0f0df8edb
Stuart Kirk <stuart.kirk@microsoft.com>
2017-12-07 17:01:28 -0500

    Updated Provision Script

--- provision-scripts/rebuild-migrate-host.sh
+++ provision-scripts/rebuild-migrate-host.sh
@@ -4,9 +4,7 @@ virsh destroy migrate-host
 virsh undefine migrate-host
 rm -f /var/lib/libvirt/images/migrate-host.qcow
 
-virt-install --name migrate-host --ram 4096 --disk path=/var/lib/libvirt/images/migrate-host.qcow,format=qcow2,size=10,bus=scsi --location=/var/lib/libvirt/images/CentOS-7-x86_64-Minimal-1708.
-iso --hvm --vcpus 2 --os-type linux --os-variant centos7.0 --network network=default --nographics --noreboot --initrd-inject=/var/lib/libvirt/images/migrate-host-ks.cfg --extra-args 'console=t
-tyS0,115200n8 serial ks=file:/migrate-host-ks.cfg'
+virt-install --name migrate-host --ram 4096 --disk path=/var/lib/libvirt/images/migrate-host.qcow,format=qcow2,size=10,bus=scsi --location=/var/lib/libvirt/images/CentOS-7-x86_64-Minimal-1708.iso --hvm --vcpus 2 --os-type linux --os-variant centos7.0 --network network=default --nographics --noreboot --initrd-inject=/var/lib/libvirt/images/migrate-host-ks.cfg --extra-args 'console=ttyS0,115200n8 serial ks=file:/migrate-host-ks.cfg'
 
 virsh autostart migrate-host
 virsh start migrate-host
